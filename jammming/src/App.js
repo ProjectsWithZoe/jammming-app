818a9f391f775203a2449e0dc48072ac7999fb99
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-11-24 16:10:12 +0000

    States created for search term and search results

--- jammming/src/App.js
+++ jammming/src/App.js
@@ -5,6 +5,33 @@ import Tracklist from "./components/Tracklist/tracklist";
 impo;
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = () => {
+    const mockSearchResults = [
+      {
+        id: "1",
+        name: "Search Song 1",
+        artist: "Search Artist 1",
+        album: "Search Album 1",
+      },
+      {
+        id: "2",
+        name: "Search Song 2",
+        artist: "Search Artist 2",
+        album: "Search Album 2",
+      },
+      {
+        id: "3",
+        name: "Search Song 3",
+        artist: "Search Artist 3",
+        album: "Search Album 3",
+      },
+    ];
+    setSearchResults(mockSearchResults);
+  };
+
   const mockTracks = [
     { id: "1", name: "Song 1", artist: "Artist 1", album: "Album 1" },
     { id: "2", name: "Song 2", artist: "Artist 2", album: "Album 2" },
